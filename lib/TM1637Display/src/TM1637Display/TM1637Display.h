779eec024a1a4a121a95f3d7dd6ccccb0c56e052
Nils Brenkman <cfbrenkman@gmail.com>
2020-02-28 03:20:04 +0100

    display

--- lib/TM1637Display/src/TM1637Display/TM1637Display.h
+++
@@ -1,1 +0,0 @@
-#include "../TM1637Display.h"
--- src/BurnHat.ino
+++ src/BurnHat.ino
@@ -6,18 +6,39 @@
  */
 
 #include "application.h"
+#include "TM1637.h"
 
 #define ROTARYPIN A0
+#define CLK A4
+#define DIO A5
+
+void displayNumber(unsigned int number);
+
+TM1637 display(CLK, DIO);
+int8_t digits[] = {0x00, 0x00, 0x00, 0x00};
 
 void setup() {
   Serial.begin(9600);
+  display.set();
+  display.init();
 }
 
 void loop() {
   int analogValue = analogRead(ROTARYPIN);
   int voltage = 3300 * analogValue / 4096;
+  displayNumber(voltage);
   Serial.println(voltage);
   delay(1000);
 }
 
-
+void displayNumber(unsigned int number) {
+  if (number > 9999) {
+    number = 9999;
+  }
+  
+  digits[0] = number / 1000;
+  digits[1] = number % 1000 / 100;
+  digits[2] = number % 100 / 10;
+  digits[3] = number % 10;
+  display.display(digits);
+}
